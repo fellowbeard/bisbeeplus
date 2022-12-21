c439d5e4500088c96c710fd69b6ecd741b1d67f5
fellowbeard <virgovibe@gmail.com>
2022-12-21 09:48:42 -0600

    adds Detail component with styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Detail from "./components/Detail";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="detail" element={<Detail />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Detail.js
@@ -0,0 +1,149 @@
+import React from "react";
+import styled from "styled-components";
+
+function Detail() {
+  return (
+    <Container>
+      <Background>
+        <img
+          src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/4F39B7E16726ECF419DD7C49E011DD95099AA20A962B0B10AA1881A70661CE45/scale?width=1440&aspectRatio=1.78&format=jpeg"
+          alt=""
+        />
+      </Background>
+      <ImageTitle>
+        <img
+          src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/D7AEE1F05D10FC37C873176AAA26F777FC1B71E7A6563F36C6B1B497CAB1CEC2/scale?width=1440&aspectRatio=1.78"
+          alt=""
+        />
+      </ImageTitle>
+      <Controls>
+        <PlayButton>
+          <img src="/images/play-icon-black.png" alt="" />
+          <span>PLAY</span>
+        </PlayButton>
+        <TrailerButton>
+          <img src="/images/play-icon-white.png" alt="" />
+          <span>TRAILER</span>
+        </TrailerButton>
+        <AddButton>
+          <span>+</span>
+        </AddButton>
+        <GroupWatchButton>
+          <img src="/images/group-icon.png" alt="" />
+        </GroupWatchButton>
+      </Controls>
+    </Container>
+  );
+}
+
+export default Detail;
+
+const Container = styled.div`
+  min-height: calc(100vh-250px);
+  padding: 0 calc(3.5vw + 5px);
+  position: relative;
+  overflow: hidden;
+  display: block;
+  top: 72px;
+`;
+
+const Background = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  opacity: 0.8;
+  z-index: -1;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
+const ImageTitle = styled.div`
+  height: 30vh;
+  min-height: 170px;
+  width: 35vx;
+  min-width: 200px;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+`;
+
+const Controls = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const PlayButton = styled.button`
+  font-size: 15px;
+  margin: 0px 22px 0px 0px;
+  padding: 0px 24px;
+  height: 56px;
+  border-radius: 4px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  letter-spacing: 1.8px;
+  text-align: center;
+  text-transform: uppercase;
+  background: rgb (249, 249, 249);
+  border: none;
+  color: rgb(0, 0, 0);
+
+  &:hover {
+    background: rgb(198, 198, 198);
+  }
+`;
+const TrailerButton = styled(PlayButton)`
+  background: rgba(0, 0, 0, 0.3);
+  border: 1px solid rgb(249, 249, 249);
+  color: rgb(249, 249, 249);
+`;
+const AddButton = styled.button`
+  width: 44px;
+  height: 44px;
+  margin-right: 16px;
+  diplay: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  border: 2px solid white;
+  background-color: rgba(0, 0, 0, 0.6);
+  cursor: pointer;
+
+  span {
+    font-size: 30px;
+    color: white;
+  }
+`;
+
+const GroupWatchButton = styled.div`
+  height: 44px;
+  width: 44px;
+  border-radius: 50%;
+  border: 2px solid white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  background-color: rgba(0, 0, 0, 0.6);
+
+  div {
+    height: 40px;
+    width: 40px;
+    background: rgb(0, 0, 0);
+    border-radius: 50%;
+
+    img {
+      width: 100%;
+    }
+  }
+`;
